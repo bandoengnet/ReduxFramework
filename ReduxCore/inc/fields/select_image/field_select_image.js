d66103d1ccf1642f0906a27a211a56311d15eab5
Travis <travis@travis-ci.org>
2013-12-22 02:28:46 +0000

    Update docs and files to distribute

--- ReduxCore/inc/fields/select_image/field_select_image.js
+++
@@ -1,5 +0,0 @@
-function updateSelectImage(obj, imgID) {
-    tochange = obj.value;
-    document.getElementById(imgID).src = tochange;
-    document.getElementById(imgID).style = 'visibility:show;';
-}
